7c3251b781eddcf02bc000127d04bfbbd0678176
Ronda Yummy <basrers@ukr.net>
2023-02-05 13:47:53 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <ShakeGame />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import ShakeGame from "./components/ShakeGame.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    ShakeGame,
   },
 };
 </script>
